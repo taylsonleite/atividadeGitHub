934ce9bf677830d27d667e5d31165d2339d5a793
Taylson <taylson.leite@gmail.com>
2021-05-07 22:18:15 -0300

    alterando o metodo de subtrair

--- src/br/edu/fafic/es/main/Subtracao.java
+++ src/br/edu/fafic/es/main/Subtracao.java
@@ -2,7 +2,7 @@ package br.edu.fafic.es.main;
 
 public class Subtracao {
 
-    public Integer subtrair(int a, int b){
-        return a - b;
+    public Integer subtrair(int a, int b, int c ){
+        return a - b -c;
     }
 }
